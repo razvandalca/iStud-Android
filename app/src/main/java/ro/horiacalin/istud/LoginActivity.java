fd5ec80b887f8e0e68c0da7d63e5c058a986afce
Horia-Alexandru Calin <horiaacalin@HoriaX.local>
2017-02-26 22:27:53 +0200

    dsdas

--- app/src/main/java/ro/horiacalin/istud/LoginActivity.java
+++ app/src/main/java/ro/horiacalin/istud/LoginActivity.java
@@ -206,6 +206,7 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
         // On Honeycomb MR2 we have the ViewPropertyAnimator APIs, which allow
         // for very easy animations. If available, use these APIs to fade-in
         // the progress spinner.
+        //jhjhkkjjkkj
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB_MR2) {
             int shortAnimTime = getResources().getInteger(android.R.integer.config_shortAnimTime);
 
