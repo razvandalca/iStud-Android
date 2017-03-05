823f2599eec6f495f96802006cbbe5833c348696
Horia-Alexandru Calin <horiaacalin@HoriaX.local>
2017-03-05 12:56:29 +0200

    Am adaugat tranzitie + diacritrice in strings

--- app/src/main/java/ro/horiacalin/istud/LoginActivity.java
+++ app/src/main/java/ro/horiacalin/istud/LoginActivity.java
@@ -3,6 +3,7 @@ package ro.horiacalin.istud;
 import android.animation.Animator;
 import android.animation.AnimatorListenerAdapter;
 import android.annotation.TargetApi;
+import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.support.annotation.NonNull;
 import android.support.design.widget.Snackbar;
@@ -181,11 +182,11 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
             // form field with an error.
             focusView.requestFocus();
         } else {
-            // Show a progress spinner, and kick off a background task to
-            // perform the user login attempt.
+
             showProgress(true);
-            mAuthTask = new UserLoginTask(email, password);
-            mAuthTask.execute((Void) null);
+            Intent intent = new Intent(this, EcranPrincipalActivity.class);
+            intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
+            startActivity(intent);
         }
     }
     private boolean isEmailValid(String email) {
