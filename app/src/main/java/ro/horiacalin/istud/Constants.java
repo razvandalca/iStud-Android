83998da5dabf2eaa81549a147c560b9d1f334fc1
Horia-Alexandru Calin <horiaacalin@HoriaX.local>
2017-03-05 20:02:25 +0200

    Pagina de materii - materialelayout

--- app/src/main/java/ro/horiacalin/istud/Constants.java
+++ app/src/main/java/ro/horiacalin/istud/Constants.java
@@ -9,5 +9,5 @@ public class Constants  {
     public static final int SPLASH_TIME_OUT = 1000;
     public static final String SHARED_PREF="shared_pref_user";
     public static final String SHARED_PREF_LOGIN="shared_pref_login";
-     public static String MATERIE_KEY="materie_key";
+    public static String MATERIE_KEY="materie_key";
 }
