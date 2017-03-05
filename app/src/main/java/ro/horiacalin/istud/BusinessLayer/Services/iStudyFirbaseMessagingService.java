a26eaea50796870cdbd1f7769cfda61f230c2bcc
razvan.dalca <dalca.razvan@gmail.com>
2017-03-05 20:02:26 +0200

    notifications - partially working

---
+++ app/src/main/java/ro/horiacalin/istud/BusinessLayer/Services/iStudyFirbaseMessagingService.java
@@ -0,0 +1,43 @@
+package ro.horiacalin.istud.BusinessLayer.Services;
+
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.content.Context;
+import android.graphics.Color;
+import android.media.RingtoneManager;
+import android.net.Uri;
+import android.support.v7.app.NotificationCompat;
+import android.util.Log;
+
+import com.google.firebase.messaging.FirebaseMessagingService;
+import com.google.firebase.messaging.RemoteMessage;
+
+/**
+ * Created by Razvan'S PC on 05.03.2017.
+ */
+
+public class iStudyFirbaseMessagingService extends FirebaseMessagingService {
+
+    @Override
+    public void onMessageReceived(RemoteMessage remoteMessage) {
+        super.onMessageReceived(remoteMessage);
+        Uri uri= RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
+        Log.e("NOTIFICATIONS", "onMessageReceived: ");
+        NotificationCompat.Builder mBuilder = new NotificationCompat.Builder(this);
+        mBuilder.setContentTitle("iStudy Mesaj Important!");
+        mBuilder.setSound(uri);
+        mBuilder.setDefaults(Notification.DEFAULT_VIBRATE);
+        mBuilder.setColor(Color.RED);
+
+
+
+        // Check if message contains a notification payload.
+        if (remoteMessage.getNotification() != null) {
+            mBuilder.setContentText(remoteMessage.getNotification().getBody());
+        }
+
+        NotificationManager notificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+        notificationManager.notify(0, mBuilder.build());
+
+    }
+}
